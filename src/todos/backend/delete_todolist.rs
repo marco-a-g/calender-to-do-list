401978aa4d2196bdd849a968511ef39acc85b416
Marco Gilio <gilio.marco@yahoo.de>
2026-01-31 15:38:14 +0100

    added delete_todo_list funtion

--- src/todos/backend/delete_todolist.rs
+++ src/todos/backend/delete_todolist.rs
@@ -7,22 +7,22 @@ use reqwest::StatusCode;
 use uuid::Uuid;
 
 // #[server]
-pub async fn delete_todo_event(todo_id: Uuid) -> Result<StatusCode, ServerFnError> {
-    println!("Starting delete_todo_event for:'{}'", todo_id);
+pub async fn delete_todo_list(list_id: Uuid) -> Result<StatusCode, ServerFnError> {
+    println!("Starting delete_todo_list for:'{}'", list_id);
 
     let (_user_id_str, token) = match get_user_id_and_session_token().await {
         Ok(data) => data,
         Err(e) => {
-            println!("Error: Not auhed");
+            println!("Error: Not authed");
             return Err(e);
         }
     };
 
     // HTTP Config
-    let url_delete = format!("{}/rest/v1/todo_events?id=eq.{}", SUPABASE_URL, todo_id);
+    let url_delete_list = format!("{}/rest/v1/todo_lists?id=eq.{}", SUPABASE_URL, list_id);
     let client = reqwest::Client::new();
     let response_result = client
-        .delete(&url_delete)
+        .delete(&url_delete_list)
         .bearer_auth(token)
         .header("apikey", ANON_KEY)
         .header("Content-Type", "application/json")
@@ -35,15 +35,15 @@ pub async fn delete_todo_event(todo_id: Uuid) -> Result<StatusCode, ServerFnErro
             let status = response.status();
             if !status.is_success() {
                 let error_text = response.text().await.unwrap_or_default();
-                println!("Supabase respons error: {}", error_text);
+                println!("Supabase response error: {}", error_text);
                 return Err(ServerFnError::new(format!(
                     "Supabase Error {}: {}",
                     status, error_text
                 )));
             }
-            println!("Deleted ToDo in Supabase.");
+            println!("Deleted ToDo-List in Supabase.");
             if let Err(e) = sync_local_to_remote_db().await {
-                println!("Error on sync after delete_todo: {:?}", e);
+                println!("Error on sync after delete_list: {:?}", e);
             }
             Ok(status)
         }
